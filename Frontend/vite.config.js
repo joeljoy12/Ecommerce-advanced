780416923acae4d7b9afcef63fb63bf1edac4947
joeljoy12 <joeljoy876@gmail.com>
2025-06-30 07:09:41 +0530

    fix 6

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
+import { createRequire } from 'module'; // Added to correct the issue
+import tailwindcss from '@tailwindcss/postcss'
 import dotenv from 'dotenv';
 
+dotenv.config();
 
-
- const env = dotenv.config().parsed;
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
   ],
-  base: env.VITE_BASE_PATH || "Ecommerce-advanced",
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+      ],
+    },
+  },
+  base: process.env.VITE_BASE_PATH || "Ecommerce-advanced", // Adjusted to use process.env
 });
